78d349783c9c23aed3def8cc87dab50c2c79f49e
australiansteve <steve@australiansteve.com>
2016-09-14 22:41:02 -0300

    FIVEANDDIME: Specific JS and CSS changes to get background #3 spinning and resizing on all screen sizes

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,14 @@ jQuery(document).ready(function($) {
 
 
 	setTimeout(function() {
-		var spaceFooter = _.debounce(footerSpacing, 500);
+		var spaceFooter = _.debounce(footerSpacing, 250);
 		$(window).resize(spaceFooter);
 		spaceFooter();
-	}, 500);
 
-	setTimeout(function() {
+		var resizeSpinner = _.debounce(spinBackground, 250);
+		$(window).resize(resizeSpinner);
+		resizeSpinner();
+
 		$("#background-div.scrolling").each(function() {
 			var backgroundHeight = $(this).css('height');
 			console.log("backgroundHeight: " + backgroundHeight);
@@ -17,6 +19,7 @@ jQuery(document).ready(function($) {
 			//Set the first background image to be the same height so that it repeats vertically
 			$(this).find("#bgImage1").css('height', backgroundHeight);
 		});
+
 	}, 500);
 
 });
@@ -30,4 +33,45 @@ function footerSpacing()
 	{
 		jQuery("#spacer").css("margin-top", marginNeeded);
 	}
+}
+
+var initialBackgroundHeight;
+
+function spinBackground()
+{
+	jQuery("#bgImage3").each(function() {
+		
+		//Get the height & width of the background image initially. This will vary by screen size
+		var existingHeight = jQuery(this).outerHeight();
+		var windowWidth = jQuery(window).width();
+
+		initialBackgroundHeight = (initialBackgroundHeight === undefined) ? existingHeight : initialBackgroundHeight;
+
+		//New height based on the proportions of the original image (1200x810px)
+		var spinningHeight = Math.floor((windowWidth / 1200) * 810);
+
+		//Only update the height if the new height is smaller than the original
+		if (spinningHeight < initialBackgroundHeight)
+		{
+			//console.log('New height ' + spinningHeight + 'px');
+			jQuery(this).css('height', spinningHeight + 'px');
+		}
+		else if (spinningHeight >= initialBackgroundHeight)
+		{
+			//console.log('Revert to initial height ' + initialBackgroundHeight + 'px');
+			jQuery(this).css('height', initialBackgroundHeight + 'px');
+		}
+
+		//Always set the width
+		jQuery(this).css('width', spinningHeight + 'px');
+
+		//Change the background size to cover, rather than contain. This will make it expand to the sizes we just set
+		jQuery(this).css('background-size', 'cover');
+
+		//Add the spinning animations
+		jQuery(this).css('-webkit-animation-name', 'spin');
+		jQuery(this).css('-moz-animation-name', 'spin');
+		jQuery(this).css('-ms-animation-name', 'spin');
+		jQuery(this).css('animation-name', 'spin');
+	});
 }
