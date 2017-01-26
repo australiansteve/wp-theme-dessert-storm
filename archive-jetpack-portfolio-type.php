48de6b5f12810509545d6204667d395008aa5727
Steve weaver Crawford <steve@australiansteve.com>
2017-01-25 22:19:35 -0400

    add styling of JetPack portfolio archive, no need to use my own Projects plugin????

---
+++ archive-jetpack-portfolio-type.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Dessertstorm
+ */
+
+get_header(); ?>
+
+<div class="row"><!-- .row start -->
+
+	<div class="small-12 columns"><!-- .columns start -->
+
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
+
+				<div class="row small-up-1 medium-up-2 large-up-3">
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php 
+
+	            		get_template_part('page-templates/partials/jetpack-portfolio', 'archive');
+
+					?>
+
+				<?php endwhile; ?>
+
+				</div>
+
+				<?php the_posts_navigation(); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'page-templates/partials/content', 'none' ); ?>
+
+			<?php endif; ?>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+	</div><!-- .columns end -->
+
+</div><!-- .row end -->
+
+<?php get_footer(); ?>
