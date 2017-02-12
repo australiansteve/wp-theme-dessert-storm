2c0f2a9642a86df75042270c54e2c669a00babc5
Steve weaver Crawford <steve@australiansteve.com>
2017-02-12 17:52:58 -0400

    Add basic override of woocommerce completed order email to include common event info. Consider putting this innto a theme customization field

---
+++ woocommerce/templates/emails/customer-completed-order.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Customer completed order email
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-completed-order.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates/Emails
+ * @version     2.5.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * @hooked WC_Emails::email_header() Output the email header
+ */
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<p><?php printf( __( "Hi there. Your recent order on %s has been completed. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) ); ?></p>
+
+<?php
+
+/**
+ * @hooked WC_Emails::order_details() Shows the order details table.
+ * @hooked WC_Emails::order_schema_markup() Adds Schema.org markup.
+ * @since 2.5.0
+ */
+do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * @hooked WC_Emails::order_meta() Shows order meta data.
+ */
+do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+?>
+
+<h2>On event night</h2>
+<p><strong>Arrival:</strong> Please arrive at least 15 minutes prior to the event's start time to allow time to sign in, order drinks, and get settled in as painting will begin promptly at the scheduled start time.
+
+<p><strong>Seating:</strong> seating is first-come, first-served, so please come early if you’re with a group or if sitting beside your pal is a must.
+
+<p><strong>Event Length:</strong> it will only take about 2 hours to complete your ready-to-hang masterpiece!
+
+<p><strong>Age Requirement:</strong> Adult event (19 and over).
+
+<p><strong>Dress code:</strong> While it’s important to look cute, painting can get messy – dress accordingly.
+
+<?php
+/**
+ * @hooked WC_Emails::customer_details() Shows customer details
+ * @hooked WC_Emails::email_address() Shows email address
+ */
+do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * @hooked WC_Emails::email_footer() Output the email footer
+ */
+do_action( 'woocommerce_email_footer', $email );
