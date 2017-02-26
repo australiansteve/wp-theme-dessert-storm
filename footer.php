a79082c7d63b56579e47e28192f361ce06e00d00
Steve weaver Crawford <steve@australiansteve.com>
2017-02-26 18:37:17 -0400

    Add 'miminal section height' option to theme customization (home page only)

--- footer.php
+++ footer.php
@@ -37,6 +37,46 @@
 	} 
 ?>
 
+<?php
+	$spaceSections = get_theme_mod('austeve_general_section_height', false);
+	if (is_home() && $spaceSections)
+	{
+?>
+	<script>
+function sectionSpacing()
+{
+	var windowHeight = Number(jQuery(window).height());
+	console.log("Window height: " + windowHeight);
+
+	jQuery("#primary.index .content-section").each(function() {
+		var rawHeight = jQuery(this).css("height")
+		var sectionHeight = Number(rawHeight.substr(0, rawHeight.length - 2));
+		console.log(jQuery(this).attr("id") + ": " + sectionHeight);
+
+		if (sectionHeight > 0 && sectionHeight < windowHeight)
+		{
+			var contentPadding = (windowHeight - sectionHeight) / 2;
+			console.log("Content padding top & bottom: " + contentPadding + "px");
+			jQuery(this).find(".content-container").css( { "padding-top" : contentPadding + "px", "padding-bottom" : contentPadding + "px"})
+		}
+
+	});
+}
+
+setTimeout(function() {
+	var spaceSections = _.debounce(sectionSpacing, 100);
+	jQuery(window).resize(spaceSections);
+	spaceSections();
+}, 100);
+
+	</script>
+
+<?php
+
+	}
+
+?>
+
 <?php wp_footer(); ?>
 </body>
 </html>
