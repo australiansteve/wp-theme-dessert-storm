04217bec5212a8a10ca80571d4c5b067d9668625
Steve weaver Crawford <steve@australiansteve.com>
2017-02-27 10:46:48 -0400

    Add 'smooth scroll' feature to scroll from section to section on the homepage only

--- footer.php
+++ footer.php
@@ -41,26 +41,28 @@
 
 <?php
 	$spaceSections = get_theme_mod('austeve_general_section_height', false);
-	if (is_home() && $spaceSections)
+	$scrollSections = get_theme_mod('austeve_general_section_scroll', false);
+	if (is_home())
 	{
 
-		$sections = get_theme_mod('austeve_general_sections', 0);
-
-		$customSpacings = array();
-		for ($s = 0; $s < $sections; $s++) 
+		if ($spaceSections)
 		{
-			$sectionName = get_theme_mod('dessertstorm_content_'.$s.'_name', null);
-			if (!$sectionName)
+			$sections = get_theme_mod('austeve_general_sections', 0);
+
+			$customSpacings = array();
+			for ($s = 0; $s < $sections; $s++) 
 			{
-				$sectionName = $s;
+				$sectionName = get_theme_mod('dessertstorm_content_'.$s.'_name', null);
+				if (!$sectionName)
+				{
+					$sectionName = $s;
+				}
+				$sectionId = strtolower(str_replace(' ', '-', $sectionName));
+
+				//Custom spacing
+				$customSpacings['section-'.$sectionId] = empty(get_theme_mod('dessertstorm_content_'.$s.'_spacing', '50')) ? '50' : get_theme_mod('dessertstorm_content_'.$s.'_spacing', '50');
 			}
-			$sectionId = strtolower(str_replace(' ', '-', $sectionName));
 
-			//Custom spacing
-			$customSpacings['section-'.$sectionId] = empty(get_theme_mod('dessertstorm_content_'.$s.'_spacing', '50')) ? '50' : get_theme_mod('dessertstorm_content_'.$s.'_spacing', '50');
-		}
-
-		error_log(print_r($customSpacings, true));
 ?>
 	<script>
 
@@ -93,8 +95,77 @@
 		}, 50);
 
 	</script>
+		<?php
+		} //END if ($spaceSections)
+
+		if ($scrollSections)
+		{
+		?>
+	<script>
+
+		function scrollIt(e) {
+			var delta = e.originalEvent.deltaY;
+			var windowHeight = Math.round(Number(jQuery(window).height()));
+			var yPosition = Math.round(window.scrollY);
+			var windowBottom = yPosition + windowHeight;
+
+			//console.log("Current position: " + yPosition + ". Bottom " + windowBottom);
+			//console.log("Window height: " + windowHeight);
+
+
+			jQuery("#primary.index .content-section").each(function() {
+				var sectionTop = Math.round(jQuery(this).offset().top);
+				var sectionBottom = sectionTop + jQuery(this).height();
+				var sectionHeight = sectionBottom - sectionTop;
+				//console.log(jQuery(this).attr("id") + ": " + sectionTop + "; " + sectionBottom);
+				//console.log("Section height" +  sectionHeight);
+
+				if (delta > 0) 
+				{
+					//console.log('down');
+					if ( windowBottom < sectionBottom || ( sectionHeight > (windowHeight + 1) && windowBottom < (sectionBottom + windowHeight * 0.2)))
+					{
+						//console.log("Bottom of section is above bottom of screen, OR tall section AND we've scrolled more than 20% past it");
+						if ( yPosition < sectionTop )
+						{
+							//console.log("Top of section is not at top of screen - SCROLL");
+							jQuery('html, body').animate({
+							    scrollTop: jQuery(this).offset().top
+							}, 1000);
+						}
+						//else
+						//{
+						//	console.log("Top of section is at or above top of screen - NO SCROLL");
+						//}
+						return false;
+					}
+				}
+				else 
+				{
+					//console.log('up');
+					if ( sectionTop < yPosition && sectionBottom > yPosition  )
+					{
+						if ( sectionHeight <= (windowHeight + 1) ) //windowHeight +1 to allow for rounding
+						{
+							jQuery('html, body').animate({
+							    scrollTop: jQuery(this).offset().top
+							}, 1000);
+						}
+						return false;
+					}
+				}
+
+			});
+		}
+
+		var autoScroll = _.debounce(scrollIt, 300);
+		jQuery(window).on('wheel', autoScroll);
+
+	</script>
 <?php
-	}
+		} //END if ($scrollSections)
+		
+	} //END if (is_home())
 ?>
 </body>
 </html>
