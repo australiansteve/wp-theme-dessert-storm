7a8d71bdf3b6946801da889e894340e10c86db89
Steve weaver Crawford <steve@australiansteve.com>
2017-04-23 17:03:19 -0300

    Styling of login page

--- functions.php
+++ functions.php
@@ -346,3 +346,15 @@ function austeve_wc_remove_password_strength() {
 	}
 }
 add_action( 'wp_print_scripts', 'austeve_wc_remove_password_strength', 100 );
+
+function austeve_login_logo() { 
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+	$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+	?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo $image[0];?>);
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'austeve_login_logo' );
