618714c644508b11b3b9fa423a00f661b7bc744b
Steve weaver Crawford <steve@australiansteve.com>
2017-05-06 17:37:12 -0300

    Remove sidebar on single and archive pages. Fix footer font size & jetpack styling

--- archive.php
+++ archive.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 <div class="row">
 
-	<div class="medium-8 columns">
+	<div class="small-12 columns">
 
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
@@ -48,12 +48,6 @@ get_header(); ?>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
-
-	</div><!-- .columns -->
-
-	<div class="medium-4 columns">
-
-		<?php get_sidebar(); ?>
 
 	</div><!-- .columns -->
 
--- single.php
+++ single.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 <div class="row">
 
-	<div class="medium-8 columns">
+	<div class="small-12 columns">
 
 		<div id="primary" class="content-area single">
 			<main id="main" class="site-main" role="main">
@@ -33,12 +33,6 @@ get_header(); ?>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
-
-	</div><!-- .columns -->
-
-	<div class="medium-4 columns">
-
-		<?php get_sidebar(); ?>
 
 	</div><!-- .columns -->
 
