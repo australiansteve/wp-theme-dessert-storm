9b118c2439c8be33060667886f48468311856594
Steve weaver Crawford <steve@australiansteve.com>
2017-10-26 21:53:47 -0300

    Remove duplicate enqueue'ing present since last heisenberg update

--- src/setup.php
+++ src/setup.php
@@ -136,25 +136,11 @@ add_action( 'wp_enqueue_scripts', function () {
  */
 add_action( 'wp_enqueue_scripts', function () {
 
-	// Add Foundation JS to footer
-	wp_enqueue_script( 'foundation-js',
-		get_template_directory_uri() . '/assets/dist/js/foundation.js',
-		array( 'jquery' ), '6.2.1', true
-	);
-
 	//Add underscore JS library
 	wp_enqueue_script( 'underscore-js', 
 		get_template_directory_uri() . '/node_modules/underscore/underscore-min.js', 
 		array( 'jquery' ), '1.8.3', true 
 	);
-
-	// Add our concatenated JS file after Foundation
-	$handle = 'dessertstorm_appjs';
-	$src =  get_template_directory_uri() . '/assets/dist/js/app.js';
-	$deps = array( 'jquery' );
-	$ver = filemtime( get_template_directory() . '/assets/dist/js/app.js');
-	$in_footer = true;
-	wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
