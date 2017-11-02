c4e9be32786125143e9313e14ab380af9c10e882
Steve weaver Crawford <steve@australiansteve.com>
2017-11-01 23:05:25 -0300

    Single post layout and styling improvments

--- src/template-tags.php
+++ src/template-tags.php
@@ -22,49 +22,31 @@ function dessertstorm_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'dessertstorm' ),
+		esc_html_x( '%s', 'post date', 'dessertstorm' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'dessertstorm' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 }
 
 /**
  * Prints HTML with meta information for the categories, tags and comments.
  */
-function dessertstorm_entry_footer() {
+function dessertstorm_entry_tags() {
 
 	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'dessertstorm' ) ); // WPCS: XSS OK.
-		if ( $categories_list && dessertstorm_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'dessertstorm' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
-
+	if ( 'post' === get_post_type() ) {		
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', __( ', ', 'dessertstorm' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . __( 'Tagged %1$s', 'dessertstorm' ) . '</span>', $tags_list );
+			printf( '<span class="tags-links">' . __( '%1$s', 'dessertstorm' ) . '</span>', $tags_list );
 		}
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( esc_html__( 'Leave a comment', 'dessertstorm' ), esc_html__( '1 Comment', 'dessertstorm' ), esc_html__( '% Comments', 'dessertstorm' ) );
-		echo '</span>';
 	}
 
 	edit_post_link(
 		sprintf(
 			/* translators: %s: Name of current post */
-			esc_html__( 'Edit %s', 'dessertstorm' ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			esc_html__( 'Edit Post', 'dessertstorm' )
 		),
 		'<span class="edit-link">',
 		'</span>'
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,15 +10,47 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		
+		<div class="row align-middle">
 
-		<div class="entry-meta">
-			<?php dessertstorm_posted_on(); ?>
-		</div><!-- .entry-meta -->
+			<div class="small-12 medium-6 columns">
+
+				<?php
+					// check if the post has a Post Thumbnail assigned to it.
+					if ( has_post_thumbnail() ) {
+						the_post_thumbnail('medium_large', array('class' => 'post-image'));
+					} 
+				?>
+
+			</div>
+
+			<div class="small-12 medium-6 columns">
+				<div class="entry-meta">
+					<?php dessertstorm_posted_on(); ?> <?php dessertstorm_entry_tags(); ?>
+				</div><!-- .entry-meta -->
+				
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+				<div class="entry-excerpt">
+					<?php the_excerpt( ); ?>					
+				</div>
+
+			</div>
+		</div>
+		
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php the_content(); ?>
+
+		<div class="row align-center">
+
+			<div class="column small-10 medium-8">
+
+				<?php the_content(); ?>
+
+			</div>
+		</div>
+
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dessertstorm' ),
@@ -27,7 +59,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php dessertstorm_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
